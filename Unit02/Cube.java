f94ee972d47cc352edbccb81545227a41110f47a
Anush-Rajan <rajana2740@my.sduhsd.net>
2022-08-22 20:10:03 -0700

    Unit2

---
+++ Unit02/Cube.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		int answer = 6 * side * side;
+		return answer;
+	}
+}
---
+++ Unit02/Trap.java
@@ -0,0 +1,16 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static float area( int base1, int base2, int height )
+	{
+		float basesum = base1 + base2;
+		float baseavg = basesum/2;
+		float result = baseavg*height;
+		return result;
+	}
+}
