82bb80f3bc2c9b89b13fdb4d50693b21ae6a1a1c
Anush-Rajan <rajana2740@my.sduhsd.net>
2022-09-14 22:46:25 -0700

    unit8

--- Unit08/RayOddToEven.java
+++ Unit08/RayOddToEven.java
@@ -5,8 +5,32 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int evenindex = -1;
+		int oddindex = -1;
+		
+		for (int i = 0; i < ray.length; i++) {
+			
+			if (oddindex >= 0 && evenindex >= 0) {
+				return evenindex - oddindex;
+			}
+			
+			if ((oddindex < 0) && (ray[i] % 2 == 1)) {
+				oddindex = i;
+				continue;
+			}
+			
+			if (((evenindex < 0) && (oddindex >= 0))  && (ray[i] % 2 == 0)) {
+				evenindex = i;
+				continue;
+			}
+			
+			
+		}
+		
+		return -1;
+
 	}
+	
 }
