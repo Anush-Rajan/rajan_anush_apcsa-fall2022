10f62ace2634f2c8f95a46c9d984353e3db02775
Anush-Rajan <rajana2740@my.sduhsd.net>
2022-12-16 23:45:35 -0800

    Unit17

---
+++ Unit17/Locatable.java
@@ -0,0 +1,15 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Anush Rajan
+//Date - 12/16/22
+//Class - APCSA
+//Lab  -
+
+public interface Locatable
+{
+    public void setPos( int x, int y);
+    public void setX( int x );
+    public void setY( int y );
+
+    public int getX();
+    public int getY();
+}
